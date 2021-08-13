b35f66fe563bb89f23488c137d300d9249710973
Niklas Fritzsche <n.fritzsche-2006@web.de>
2021-08-13 14:08:59 +0200

    Started adding Homework files and Vue Components to display them.

---
+++ src/views/Homework.vue
@@ -0,0 +1,27 @@
+<template>
+  <ol>
+    <li>Task
+      <ul>
+        <li><a href='http://www2.inf.h-brs.de/~nfrit12s/Components/Uebung_1/task1_3.html' target="_blank">Task 1.3</a></li>
+      </ul>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: "Homework",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeworkReview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-html="''"></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HomeworkReview",
+  data() {
+    return {
+      content: ""
+    };
+  },
+  props: [
+      'folder',
+      'task'
+  ],
+  created() {
+    this.loadFile()
+  },
+  methods: {
+    loadFile() {
+      axios({
+        method: "get",
+        url: "../assets/homework/Uebung_1/task1_3.html"
+      })
+          .then(result => {
+            this.content = result.data;
+          })
+          .catch(error => {
+            console.error("error getting file", error);
+          });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

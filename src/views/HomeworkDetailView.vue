bdbf623b144fdc6b568d14502070b3f2746baeac
Niklas Fritzsche <n.fritzsche-2006@web.de>
2021-09-17 15:20:48 +0200

    Added extraction for external JavaScript files in HomeworkDetailView.vue

--- src/views/HomeworkDetailView.vue
+++ src/views/HomeworkDetailView.vue
@@ -7,6 +7,12 @@
         <pre class="prettyprint">
             {{content}}
         </pre>
+        <div v-for="(value, index) of js_files" v-bind:key="index">
+          <h3>{{value[0]}}</h3>
+          <pre class="prettyprint">
+            {{value[1]}}
+          </pre>
+        </div>
       </div>
       <div class="iframe-div">
         <h3>Preview</h3>
@@ -22,13 +28,24 @@ export default {
   data() {
     return {
       content: "",
-      js_files: []
+      js_files: new Map()
     };
   },
   props: [
       'folder',
       'task'
   ],
+  methods: {
+    async fetchJs(path) {
+      console.log(path);
+      const response = await fetch('http://www2.inf.h-brs.de/~nfrit12s/' + path);
+
+      if (response.ok) {
+        let text = await response.text();
+        this.js_files.set(path, text);
+      }
+    }
+  },
   created() {
     let html_string = 'http://www2.inf.h-brs.de/~nfrit12s/homework/' + this.folder + '/' + this.task;
 
@@ -36,9 +53,13 @@ export default {
         .then( r => r.text())
         .then( t => {
           this.content = t;
-          let check_for_external_script = this.content.match(/<script src=(.*?)<\/script>/i);
-          if (check_for_external_script != null) {
-            console.log(check_for_external_script);
+          let check_for_external_script = this.content.matchAll(/<script src=(.*?)<\/script>/gi);
+          for (let el of check_for_external_script) {
+            let js_source = el[1]
+                .replaceAll('../', '')
+                .replaceAll('"', '')
+                .replaceAll('>', '');
+            this.fetchJs(js_source);
           }
         });
   },
@@ -46,7 +67,7 @@ export default {
     // Inject Google Code Prettifier as external Script as described here: https://vue-view.com/how-to-load-an-external-script-in-vue-component/
     let externalScript = document.createElement('script');
     externalScript.setAttribute('src',
-        'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=html&lang=css&skin=sunburst');
+        'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=html&lang=css&lang=js&skin=sunburst');
     document.head.appendChild(externalScript);
   }
 }

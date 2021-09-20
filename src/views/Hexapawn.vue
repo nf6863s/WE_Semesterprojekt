741d511c92c317aaef64f570635ce464c9ba516f
Niklas Fritzsche <n.fritzsche-2006@web.de>
2021-09-20 15:20:41 +0200

    Continued working on HexaPawn implementation: MoveList gets auto initialized on reload, moves get played by computer in response to player moves. TODO: Punish Comp for bad moves

--- src/views/Hexapawn.vue
+++ src/views/Hexapawn.vue
@@ -38,12 +38,14 @@ export default {
       pawns: [],
       selected_pawn: 0,
       highlighted_fields: [],
-      test: () => {}
+      move_pawn_function: () => {},
+      move_number: 0,
+
+      move_response_list: {}
     }
   },
   methods: {
     click_pawn(id) {
-      console.log("reached");
       // remove previously selected pawns css highlighting
       if (this.selected_pawn > 0) {
         let prevPawn = document.getElementById(this.selected_pawn);
@@ -63,8 +65,12 @@ export default {
       this.selected_pawn = id;
 
       // Get all possible moves for selected pawn
-      let legal_moves = this.get_legal_moves(id, true);
-      console.log(legal_moves);
+      let legal_moves = this.get_legal_moves(this.board, this.pawns.get(this.selected_pawn).pos, true);
+
+      if (legal_moves.length === 0) {
+        alert("game over - no moves");
+        return;
+      }
 
       // Add CSS Highlighting to fields that can be moved to and add click-event to move the pawn
       for (let move of legal_moves) {
@@ -73,53 +79,87 @@ export default {
         field.classList.add('highlight');
         this.highlighted_fields.push(id);
 
-        let move_function = () => this.move_pawn(id);
+        let move_function = () => this.move_pawn(this.selected_pawn, id, true);
 
         // Providing closure for internal function to allow click-event to handle function call
         //const move_function = this.test;
         field.addEventListener('click', move_function);
 
-        this.test = move_function;
+        this.move_pawn_function = move_function;
       }
     },
 
     init_pawns() {
       this.pawns = new Map();
-      this.pawns.set(1, {pos: [0, 0], white: true});
-      this.pawns.set(2, {pos: [0, 1], white: true});
-      this.pawns.set(3, {pos: [0, 2], white: true});
+      this.pawns.set(1, this.pawn_object_factory(1, [0, 0], true));
+      this.pawns.set(2, this.pawn_object_factory(2, [0, 1], true));
+      this.pawns.set(3, this.pawn_object_factory(3, [0, 2], true));
 
-      this.pawns.set(4, {pos: [2, 0], white: false});
-      this.pawns.set(5, {pos: [2, 1], white: false});
-      this.pawns.set(6, {pos: [2, 2], white: false});
+      this.pawns.set(4, this.pawn_object_factory(4, [2, 0], false));
+      this.pawns.set(5, this.pawn_object_factory(5, [2, 1], false));
+      this.pawns.set(6, this.pawn_object_factory(6, [2, 2], false));
     },
 
-    init_field() {
-      this.board = [[1,2,3],[0,0,0],[4,5,6]];
+    pawn_object_factory(id, pos, white) {
+      let new_pawn = {};
+      new_pawn.id = id;
+      new_pawn.pos = pos;
+      new_pawn.white = white;
+
+      new_pawn.setPos = (pos) => { new_pawn.pos = pos };
+      new_pawn.getPos = () => { return new_pawn.pos };
+
+      new_pawn.getId = () => { return new_pawn.id };
+      new_pawn.isWhite = () => { return new_pawn.white };
+
+      return new_pawn;
     },
 
-    get_legal_moves(id, white) {
-      let pawn = this.pawns.get(id);
-      let pos = pawn.pos;
+    init_board() {
+      this.board[0] = [1,2,3];
+      this.board[1] = [0,0,0];
+      this.board[2] = [4,5,6];
+    },
+
+    get_legal_moves(board, pos, white) {
       let moves = [];
 
       if (white) {
         // Check if field in front of pawn is empty and if it is, add the move to the legal move list
-        if (this.get_pawn_at_pos(pos[0] + 1, pos[1]) === 0) {
+        if (board[pos[0] + 1][pos[1]] === 0) {
           moves.push([pos[0] + 1, pos[1]]);
         }
 
-        console.log('getmoves', pos);
-
-        // Check if pawn can attack diagonally left
-        if (pos[1] > 0 && this.get_pawn_at_pos(pos[0] + 1, pos[1] - 1) !== 0) {
-          console.log("left attack");
+        // Check if pawn can attack diagonally left and is opponents Pawn
+        let pawn_at_next_pos = board[pos[0] + 1][pos[1] - 1];
+        // pawn_at_next_pos > 3 = black pawns
+        if (pos[1] > 0 && pawn_at_next_pos !== 0 && pawn_at_next_pos > 3) {
           moves.push([pos[0] + 1, pos[1] - 1]);
         }
 
-        // Check if pawn can attack diagonally right
-        if (pos[1] < 2 && this.get_pawn_at_pos(pos[0] + 1, pos[1] + 1) !== 0) {
+        pawn_at_next_pos = board[pos[0] + 1][pos[1] + 1];
+        // Check if pawn can attack diagonally right and is opponents Pawn
+        if (pos[1] < 2 && pawn_at_next_pos !== 0 && pawn_at_next_pos > 3) {
           moves.push([pos[0] + 1, pos[1] + 1]);
+        }
+      } else {
+        // Legal moves for Black:
+        // Check if field in front of pawn is empty and if it is, add the move to the legal move list
+        if (board[pos[0] - 1][pos[1]] === 0) {
+          moves.push([pos[0] - 1, pos[1]]);
+        }
+
+        // Check if pawn can attack diagonally left and is opponents Pawn
+        let pawn_at_next_pos = board[pos[0] - 1][pos[1] - 1];
+        // pawn_at_next_pos < 4 = white pawns
+        if (pos[1] > 0 && pawn_at_next_pos !== 0 && pawn_at_next_pos < 4) {
+          moves.push([pos[0] - 1, pos[1] - 1]);
+        }
+
+        pawn_at_next_pos = board[pos[0] - 1][pos[1] + 1];
+        // Check if pawn can attack diagonally right and is opponents Pawn
+        if (pos[1] < 2 && pawn_at_next_pos !== 0 && pawn_at_next_pos < 4) {
+          moves.push([pos[0] - 1, pos[1] + 1]);
         }
       }
 
@@ -141,51 +181,258 @@ export default {
         let field = document.getElementById(id);
         field.classList.remove('highlight');
 
-        const move_function = this.test;
+        const move_function = this.move_pawn_function;
         field.removeEventListener('click', move_function);
       }
 
       this.highlighted_fields = [];
     },
 
-    move_pawn(id) {
+    move_pawn(pawn_to_move, id, white) {
       // Use this.selected_pawn as indicator which pawn should be moved
-      console.log("test", id, this.selected_pawn);
       let field = document.getElementById(id);
 
+      // Coordinates as ints
+      let x = parseInt(id[0]);
+      let y = parseInt(id[1]);
+
+
       // Check if a pawn has been taken
-      let pawn_at_position = this.get_pawn_at_pos(id[0], id[1]);
+      let pawn_at_position = this.get_pawn_at_pos(x, y);
 
       if (pawn_at_position !== 0) {
         this.pawns.delete(pawn_at_position);
         document.getElementById(pawn_at_position).remove();
       }
 
-      let pawn_prev_pos = document.getElementById(this.selected_pawn);
-      pawn_prev_pos.parentElement.classList.remove('selected');
-      pawn_prev_pos.remove();
-      field.appendChild(pawn_prev_pos);
+      let pawn_prev_pos = document.getElementById(pawn_to_move);
+      if (pawn_prev_pos !== null) {
+        pawn_prev_pos.parentElement.classList.remove('selected');
+        pawn_prev_pos.remove();
+        field.appendChild(pawn_prev_pos);
+      }
 
-      let pawn = this.pawns.get(this.selected_pawn);
-      let oldPos = pawn.pos;
-      let newPos = [id[0], id[1]];
+      let pawn = this.pawns.get(pawn_to_move);
+      let oldPos = pawn.getPos();
+      let newPos = [x, y];
 
       this.board[oldPos[0]][oldPos[1]] = 0;
-      this.board[newPos[0]][newPos[1]] = this.selected_pawn;
+      this.board[x][y] = pawn_to_move;
 
-      pawn.pos = newPos;
-
-      console.log(this.pawns.get(this.selected_pawn));
-
-      console.log(this.board);
+      pawn.setPos(newPos);
 
       this.remove_highlighting();
       this.selected_pawn = 0;
+
+      this.move_number++;
+
+      // Check if player has won
+      let game_over = this.check_win_conditions();
+
+      if (game_over) {
+        alert("game over - You Win");
+      }
+      if (white) {
+        this.make_opponent_move();
+      }
+    },
+
+    check_win_conditions() {
+
+      // 1. Win Condition: Pawn at opponents end of the board
+      for (let i = 0; i < 3; i++) {
+        // Check for black pawns at white side of the board
+        let pawn_id = this.get_pawn_at_pos(0, i);
+
+        if (pawn_id > 0 && !this.pawns.get(pawn_id).isWhite()) {
+          return true;
+        }
+
+        // Check for white pawns at black side of the board
+        pawn_id = this.get_pawn_at_pos(2, i);
+
+        if (pawn_id > 0 && this.pawns.get(pawn_id).isWhite()) {
+          return true;
+        }
+      }
+
+      // 2. Win Condition: Opponent has no more legal moves
+      // implementing in click_pawn-method
+
+      // 3. Win Condition: Opponent has no more Pawns left
+
+      let prev_pawn_color = null;
+      for (let pawn of this.pawns.values()) {
+
+        // Check if all Pawns have the same color
+        if (prev_pawn_color === null) {
+          prev_pawn_color = pawn.isWhite();
+        } else if (prev_pawn_color ^ pawn.isWhite()) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+
+    make_opponent_move() {
+      for (let i = 0; i < this.move_response_list.moves.length; i++) {
+        let move = this.move_response_list.moves[i];
+        //console.log(this.compareToCurrentBoard(move.board)? 'board found': '');
+        if (move.move === this.move_number + 1 && this.compareToCurrentBoard(move.board) && move.possible_moves.length > 0) {
+          let random_selection = parseInt(Math.random() * move.possible_moves.length);
+          let random_selected_move = move.possible_moves[random_selection];
+          console.log(move, random_selected_move);
+          this.move_pawn(random_selected_move.id, random_selected_move.move_to[0] + '' + random_selected_move.move_to[1], false);
+        } else if (move.possible_moves.length === 0){
+          // probably need to play a random legal move anyways to continue interaction with player
+          //console.log('no more moves - black looses');
+        }
+      }
+    },
+
+    init_opponent_move_list() {
+      /* initial board stat
+          1 2 3
+          0 0 0
+          4 5 6
+      */
+
+      // Shallow copy does not suffice for multi-dimensional array
+      //let test_board = [...this.board];
+      let test_board = this.deep_copy_array(this.board);
+      this.move_response_list = {"moves": []}
+
+      this.recursively_init_move_response_list(test_board, this.move_response_list, 2);
+
+      console.log(this.move_response_list.moves.sort((moveA, moveB) => {return Math.sign(moveA.move - moveB.move)}));
+      //console.log(this.get_legal_moves([[1,0,0],[0,3,0],[4,5,0]], [2, 0], false))
+    },
+
+    recursively_init_move_response_list(curr_board, move_response_list, move_depth) {
+      let test_board = this.deep_copy_array(curr_board);
+      let pawn_pos_list = this.get_pawn_positions(test_board);
+
+      // Check if one side has no more pawns
+      let white_pawns = 0;
+      let black_pawns = 0;
+
+      for (let k = 0; k < 3; k++) {
+        white_pawns += pawn_pos_list[k].length > 0 ? 1 : 0;
+        black_pawns += pawn_pos_list[k + 3].length > 0 ? 1 : 0;
+      }
+
+      if (white_pawns === 0 || black_pawns === 0) {
+        return;
+      }
+
+      for (let i = 0; i < 3; i++) {
+        // Continue if pawn with id i no longer exists
+        if (pawn_pos_list[i].length === 0) {
+          continue;
+        }
+
+        let legal_moves_white = this.get_legal_moves(test_board, pawn_pos_list[i], true);
+
+        for (let move of legal_moves_white) {
+          // Need to check, if pawn_pos_list was changed in line 348 and revert that change if possible
+          pawn_pos_list = this.get_pawn_positions(test_board);
+          let board_after_move = this.simulate_game_state(test_board, pawn_pos_list[i], move);
+
+          // Check board for white win by promotion
+          if (this.check_board_for_promotion(board_after_move)) {
+            continue;
+          }
+
+          // Need to check, if white takes a black pawn in the previous move-simulation
+          pawn_pos_list = this.get_pawn_positions(board_after_move);
+
+          let possible_moves_black = [];
+          let boards_next_depth = [];
+          for (let j = 3; j < 6; j++) {
+            // Continue if pawn with id j no longer exists
+            if (pawn_pos_list[j].length === 0) {
+              continue;
+            }
+            let legal_moves_black = this.get_legal_moves(board_after_move, pawn_pos_list[j], false);
+
+            for (let move_black of legal_moves_black) {
+              possible_moves_black.push({"id": j + 1, "move_to": move_black});
+              boards_next_depth.push(this.simulate_game_state(board_after_move, pawn_pos_list[j], move_black));
+            }
+          }
+
+          if (possible_moves_black.length > 0) {
+            move_response_list.moves.push({
+              "move": move_depth,
+              "board": board_after_move,
+              "possible_moves": possible_moves_black
+            });
+
+            if (move_depth < 6) {
+              for (let board_next_depth of boards_next_depth) {
+                // Check if black has won on this move and if it did, no need to calculate further
+                if (this.check_board_for_promotion(board_next_depth)) {
+                  continue;
+                }
+                this.recursively_init_move_response_list(board_next_depth, move_response_list, move_depth + 2);
+              }
+            }
+          }
+        }
+      }
+    },
+
+    get_pawn_positions(board) {
+      let pawn_pos_list = [[],[],[],[],[],[]];
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] > 0) {
+            pawn_pos_list[parseInt(board[i][j]) - 1] = [i, j];
+          }
+        }
+      }
+
+      return pawn_pos_list;
+    },
+
+    simulate_game_state(board, pawn_pos, move) {
+        let internal_board = this.deep_copy_array(board);
+        let pawn_id = board[pawn_pos[0]][pawn_pos[1]];
+        internal_board[pawn_pos[0]][pawn_pos[1]] = 0;
+        internal_board[move[0]][move[1]] = pawn_id;
+
+        return internal_board;
+    },
+
+    deep_copy_array(arr) {
+      return JSON.parse(JSON.stringify(arr));
+    },
+
+    check_board_for_promotion(board_after_move) {
+      for (let i = 0; i < 3; i++) {
+        if (board_after_move[0][i] > 3 || (board_after_move[2][i] < 4 && board_after_move[2][i] > 0)) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    compareToCurrentBoard(board) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (this.board[i][j] !== board[i][j]) {
+            return false;
+          }
+        }
+      }
+      return true;
     }
   },
   mounted() {
     this.init_pawns();
-    this.init_field();
+    this.init_board();
+    this.init_opponent_move_list();
   }
 }
 </script>

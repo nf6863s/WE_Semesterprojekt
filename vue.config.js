743010c1d4c3e960df19122b489373c5893ab99d
Niklas Fritzsche <n.fritzsche-2006@web.de>
2021-09-21 16:49:13 +0200

    Test for http to https request error

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/WE_Semesterprojekt/'
-        : '/'
+        : '/',
+    chainWebpack: (config) => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'WE_Semesterproject';
+                args[0].meta = {'http-equiv': 'Content-Security-Policy', 'content':'upgrade-insecure-requests'};
+
+                return args;
+            })
+    }
 }
